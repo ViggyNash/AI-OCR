6d1a8f6e73989f303aeeadc5e5ea6e2a8d4413f9
KartyN <saikarthik.nair@gmail.com>
2016-12-14 12:43:46 -0500

    Perceptron + Util

---
+++ perceptron.py
@@ -0,0 +1,78 @@
+import util
+PRINT = True
+
+class PerceptronClassifier:
+  """
+  Perceptron classifier.
+  
+  Note that the variable 'datum' in this code refers to a counter of features
+  (not to a raw samples.Datum).
+  """
+  def __init__( self, legalLabels, max_iterations):
+    self.legalLabels = legalLabels
+    self.type = "perceptron"
+    self.max_iterations = max_iterations
+    self.weights = {}
+    for label in legalLabels:
+      self.weights[label] = util.Counter() # this is the data-structure you should use
+
+  def setWeights(self, weights):
+    assert len(weights) == len(self.legalLabels)
+    self.weights == weights
+      
+  def train( self, trainingData, trainingLabels, validationData, validationLabels ):
+    """
+    The training loop for the perceptron passes through the training data several
+    times and updates the weight vector for each label based on classification errors.
+    See the project description for details. 
+    
+    Use the provided self.weights[label] data structure so that 
+    the classify method works correctly. Also, recall that a
+    datum is a counter from features to values for those features
+    (and thus represents a vector a values).
+    """
+    
+    self.features = trainingData[0].keys() # could be useful later
+    # DO NOT ZERO OUT YOUR WEIGHTS BEFORE STARTING TRAINING, OR
+    # THE AUTOGRADER WILL LIKELY DEDUCT POINTS.
+
+
+    for iteration in range(self.max_iterations):
+      print "Starting iteration ", iteration, "..."
+      for i in range(len(trainingData)):
+          "*** YOUR CODE HERE ***"
+          features = trainingData[i]
+          label = trainingLabels[i]
+          guessedLabel = self.getLabelWithMaxScore(self.weights, features)
+          if (guessedLabel != label):
+            self.weights[label] += features
+            self.weights[guessedLabel] -= features
+
+
+  def getLabelWithMaxScore(self, weights, features):
+    """
+    Helper function used for finding the label with the max score,
+    given a set of features and their weights.
+    """
+    scores = util.Counter()
+    for label in self.legalLabels:
+      scores[label] = weights[label] * features
+    return scores.argMax()
+
+
+  def classify(self, data ):
+    """
+    Classifies each datum as the label that most closely matches the prototype vector
+    for that label.  See the project description for details.
+    
+    Recall that a datum is a util.counter... 
+    """
+    guesses = []
+    for datum in data:
+      vectors = util.Counter()
+      for l in self.legalLabels:
+        vectors[l] = self.weights[l] * datum
+      guesses.append(vectors.argMax())
+    return guesses
+
+  
