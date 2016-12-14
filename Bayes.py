483d7240f5b17b8172c9a49b207289f7d5b41c39
ViggyNash <viggyviggynash@gmail.com>
2016-12-13 23:45:44 -0500

    Finished Bayes.py, updated design doc (hopefully finalized)

--- Bayes.py
+++ Bayes.py
@@ -1,9 +1,15 @@
 
-modelData = 0
-
+modelData = []	#class x feature x value
+classCounts = []
 
 def getModelData(file = 'bData.txt')
-	file = open(file, 'r')
+	try:
+		file = open(file, 'r')
+		break;
+	except IOError as e
+		print("IOError: File "+file+" not found.")
+		sys.exit()
+
 	data = file.read()
 	list = data.splitlines()
 	classCount = len(list) - 1
@@ -17,15 +23,18 @@ def getModelData(file = 'bData.txt')
 				modelData[i][j][k] = line[j * valCount + k]
 
 
-def bayesTraining(FeaturesList, LabelsList, labelMapping)
+def bayesTraining(FeaturesList, LabelsList, labelMapping, valRange)
 
 	output = ''
+	modelData = [len(labelMapping)][len(FeaturesList[0])][valRange]
+	classCounts = [len(labelMapping)]
 	
-	for i = range(LabelsList)
-		classIdx = labelMapping.index(LabelsList[i])
-		for j = range(FeaturesList)
-			for k = range(len(FeaturesList[j])
-				modelData[classIdx][j][Features[j][k]] += 1
+	for i in range(len(LabelsList)) 								#For each label
+		classIdx = labelMapping.index(LabelsList[i]) 				#Get the class index of the given label
+		classCounts[classIdx] += 1
+		for j in range(len(FeaturesList))							#For each item in the list
+			for k in range(len(FeaturesList[j]))					#For each feature the item has
+				modelData[classIdx][j][Features[j][k]] += 1			#Increment the counter of the corresponding value
 				output += str(modelData[classIdx][j][Features[j][k]]) +  ' '
 		output += '\n'
 
@@ -33,11 +42,18 @@ def bayesTraining(FeaturesList, LabelsList, labelMapping)
 	file.write(output)
 
 
-def bayesTest(inputFeatures)
+def bayesTest(inputFeatures, labelMapping)
 	
-	cProbs = [len(modelData)]
-
+	conditionalProbs = [1 for x in range(len(modelData))] # Counts for input feature value given class
 	for i = len(modelData)
 		for j = len(modelData[i])
-			cProbs[i] = modelData[i][j]
+			conditionalProbs[i][j] *= modelData[i][j][inputFeatures[j]]	#Multiply the counts of the number of times the matching value
+																		#value for a given feature of a given class was seen
+	conditionalProbs = 	[conditionalProbs[x]/classCounts[x] for x in range(len(conditionalProbs))]
 
+	totalI = sum(classCounts)
+	probI = [len(classCounts)]
+	for i in range(len(classCounts))
+		probI[i] = conditionalProbs[i] * classCounts[i] / totalI
+
+	return max(probI), labelMapping[find(max(probI))]
