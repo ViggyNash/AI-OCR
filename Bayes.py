ea4875423dea9a2102c64f9da2cb97eb4bff3d97
ViggyNash <viggyviggynash@gmail.com>
2016-12-13 22:13:45 -0500

    Additions to Bayes.py, changes to design doc

--- Bayes.py
+++ Bayes.py
@@ -2,15 +2,13 @@
 modelData = 0
 
 
-def getModelData(file)
+def getModelData(file = 'bData.txt')
 	file = open(file, 'r')
 	data = file.read()
 	list = data.splitlines()
 	classCount = len(list) - 1
 	featCount = int(list[0].strip())
 	valCount = len(list[1]) / featCount
-
-	#modelData = [x.split() for x in list]
 
 	for i in range(1, classCount)
 		line = list[i].split(' ')
@@ -19,4 +17,27 @@ def getModelData(file)
 				modelData[i][j][k] = line[j * valCount + k]
 
 
-def 
+def bayesTraining(FeaturesList, LabelsList, labelMapping)
+
+	output = ''
+	
+	for i = range(LabelsList)
+		classIdx = labelMapping.index(LabelsList[i])
+		for j = range(FeaturesList)
+			for k = range(len(FeaturesList[j])
+				modelData[classIdx][j][Features[j][k]] += 1
+				output += str(modelData[classIdx][j][Features[j][k]]) +  ' '
+		output += '\n'
+
+	file = open('bData.txt','w')
+	file.write(output)
+
+
+def bayesTest(inputFeatures)
+	
+	cProbs = [len(modelData)]
+
+	for i = len(modelData)
+		for j = len(modelData[i])
+			cProbs[i] = modelData[i][j]
+
