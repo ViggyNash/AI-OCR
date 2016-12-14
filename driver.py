2e72107ad1dd38c8c60b550618822a639b5b7149
ViggyNash <viggyviggynash@gmail.com>
2016-12-14 18:02:09 -0500

    IM BAD AT SAVING

--- driver.py
+++ driver.py
@@ -2,22 +2,22 @@ import Bayes
 import perceptron
 
 # feature extractor
-def extract(data, type)
+def extract(data, type):
 	size = [4] # height/width of sub blocks, num blocks per column/row
-	if type == 'number'
+	if type == 'number':
 		size = [7,7,4,4]	#16 total blocks
-	else if type == 'face'
+	else if type == 'face':
 		size = [10,10,7,6] 	#42 total blocks
-	else
+	else:
 		print 'Invalid type.'
 		sys.exit()
 
 	blockCounts = [size[3] * size[4]]
 
-	for i in range(size[3])
-		for j in range(size[4])
-			for k in range(size[1])
-				for l in range(size[2])
+	for i in range(size[3]):
+		for j in range(size[4]):
+			for k in range(size[1]):
+				for l in range(size[2]):
 					if data[i * size[3] + k][j * size[4] + l] != 0
 						blockCounts[i * j] += 1
 	valRange = size[1] * size[2]
@@ -28,18 +28,18 @@ def extract(data, type)
 # MAIN
 args = sys.argv
 mode = 0 # 0 = training, 1 = testing
-if len(args) == 3
+if len(args) == 3:
 	mode = 1
-if len(args) == 4
+if len(args) == 4:
 	mode = 0
-else
+else:
 	print 'Invalid number of arguments'
 	sys.exit()
 
 #get labels
-try
+try:
 	labelFile = open(args[1],'r')
-except IOError
+except IOError:
 	print "IOError: Unable to open file "+args[1]"."
 	sys.exit()
 labels = labelFile.read().split('\n')
@@ -47,9 +47,9 @@ trueLen = round(len(labels) / 10, 0) * 10
 labels = labels(:trueLen)
 
 #get data
-try
+try:
 	dataFile = open(args[0],'r')
-except IOError
+except IOError:
 	print "IOError: Unable to open file "+args[0]"."
 	sys.exit()
 data = dataFile.read().split('\n')
@@ -60,33 +60,33 @@ data = data(:trueLen)
 type = args[3].lower()
 size = 0
 labelMapping = []
-if type == 'number'
+if type == 'number':
 	size = 28
 	labelMapping = ['0','1','2','3','4','5','6','7','8','9']
-if type == 'face'
+if type == 'face':
 	size = 70
 	labelMapping = ['0','1']
-else 
+else:
 	print 'Invalid type.'
 	sys.exit()
 
 # build features list using extract function
 valRange
 featuresList = [len(data) / size]
-for i in range(len(data) / size)
+for i in range(len(data) / size):
 	featuresList[i], valRange = extract(data[i * size:i * size + size], type)
 
 assert len(featuresList)
 
 model = args[2].lower()
-if model == 'bayes'
+if model == 'bayes':
 	# Bayes
 	bayesTraining(featuresList, labels, labelMapping, valRange)
-else if model == 'percept'
+else if model == 'percept':
 	# Perceptron
-else if model == 'other'
+else if model == 'other':
 	# Other
 	print 'NOT YET IMPLEMENTED'
-else 
+else :
 	print 'Invalid model specified.'
 	sys.exit()
